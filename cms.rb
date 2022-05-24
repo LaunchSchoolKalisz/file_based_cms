8b2f8e2b4ece06c3b0fa3dce42fc57853ed2abe6
mskalisz <mskalisz3@gmail.com>
2022-05-24 16:50:12 -0400

    Read files using plain text

--- cms.rb
+++ cms.rb
@@ -9,6 +9,13 @@ get "/" do
   @files = Dir.glob(root + "/documents/*").map do |path|
     File.basename(path)
   end
-  
+
   erb :index, layout: :layout
+end
+
+get "/documents/:filename" do
+  file_path = root + "/documents/" + params[:filename]
+
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
 end
