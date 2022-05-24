92b4847d47f62e299807f99c7a2e0412f1cc7102
mskalisz <mskalisz3@gmail.com>
2022-05-24 17:28:01 -0400

    Add test for when a file is not found

--- test/app_test.rb
+++ test/app_test.rb
@@ -27,4 +27,18 @@ class AppTest < Minitest::Test
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes last_response.body, "Ruby 0.95 released"
   end
+
+  def test_document_not_found
+    get "/notafile.ext"
+
+    assert_equal 302, last_response.status
+
+    get last_response["Location"] # Request the page that the user was redirected to
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "notafile.ext does not exist"
+
+    get "/"
+    refute_includes last_response.body, "notafile.ext does not exist"
+  end
 end
