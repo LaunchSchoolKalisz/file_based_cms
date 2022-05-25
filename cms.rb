f80df358418b213ce36229e4e7afe698027ab55b
mskalisz <mskalisz3@gmail.com>
2022-05-24 21:34:51 -0400

    Convert about into a markdown file and add text

--- cms.rb
+++ cms.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 require 'sinatra/content_for'
+require 'redcarpet'
 
 root = File.expand_path("..", __FILE__)
 
---
+++ documents/about.md
@@ -0,0 +1,3 @@
+# Ruby is ...
+
+A dynamic, open source programming language with **focus** on simplicity and productivity. It has an *elegant* syntax that is natural and easy to write. 
